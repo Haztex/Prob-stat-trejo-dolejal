d63ec7a9663570cf505b2bd095491b1c0cf7aefd
Haztex <57026189+Haztex@users.noreply.github.com>
2019-11-01 21:27:09 -0600

    Update Introducing Names python exercise.py

--- Introducing Names python exercise.py
+++ Introducing Names python exercise.py
@@ -1,3 +1,5 @@
+## Introducing Names Exercise 01119
+
 values = int(input("How much data do you want to introduce?"))
 names = []
 total = 0
@@ -6,3 +8,5 @@ for i in range(0, values):
     names.append(X)
 namez = int(input("Which name do you want to know?"))
 print(names [namez])
+
+## Emerson Trejo Dolejal 3A
